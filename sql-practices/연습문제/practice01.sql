a33416696550148a89c0059088cec295df1c7cb2
daseulkim00 <ektmf8619@naver.com>
2021-10-02 10:07:25 +0900

    'practice01 solve except03'

--- sql-practices/연습문제/practice01.sql
+++ sql-practices/연습문제/practice01.sql
@@ -2,9 +2,12 @@
 -- 문제1.
 -- 사번이 10944인 사원의 이름은(전체 이름)
 
-select concat(first_name, ' ', last_name) as '이름', emp_no as '사번'
- from employees
-where emp_no like('10944');
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS '이름', emp_no AS '사번'
+FROM
+    employees
+WHERE
+    emp_no LIKE ('10944');
 
 -- 문제2. 
 -- 전체직원의 다음 정보를 조회하세요. 가장 선임부터 출력이 되도록 하세요. 
@@ -25,61 +28,79 @@ order by hire_date;
 -- 현재 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용합니다.) 
 -- 현재:to_date ='9999-01-01'
 
-select count(*)
- from salaries
-where to_date = '9999-01-01'; 
-
-
+SELECT 
+    COUNT(*)
+FROM
+    salaries
+WHERE
+    to_date = '9999-01-01'; 
 
 -- 문제5.
 -- 부서는 총 몇 개가 있나요?
 -- distin쓰고 count써라
-select distinct count(*) as '부서 수'
-	from departments;
-
+SELECT DISTINCT
+    COUNT(*) AS '부서 수'
+FROM
+    departments;
 
 -- 문제6.
 -- 현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외)
-select count(*) as '부서 매니저 수'
- from dept_manager 
- where to_date = '9999-01-01';
+SELECT 
+    COUNT(*) AS '부서 매니저 수'
+FROM
+    dept_manager
+WHERE
+    to_date = '9999-01-01';
 
 -- 문제7.
 -- 전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
-select *
- from departments
- order by length(dept_name) desc;
+SELECT 
+    *
+FROM
+    departments
+ORDER BY LENGTH(dept_name) DESC;
 
 
 -- 문제8.
 -- 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
-select count(*)
- from salaries
- where to_date = '9999-01-01'
- and salary >= '120000';
+SELECT 
+    COUNT(*)
+FROM
+    salaries
+WHERE
+    to_date = '9999-01-01'
+        AND salary >= '120000';
 
 
 -- 문제9.
 -- 어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
-select distinct title
-  from titles
- order by length(title) desc;
+SELECT DISTINCT
+    title
+FROM
+    titles
+ORDER BY LENGTH(title) DESC;
 
 
 -- 문제10
 -- 현재 Enginner 직책의 사원은 총 몇 명입니까?
-select count(*) 
-  from titles
- where  title = 'Enginner' 
- and to_date = '9999-01-01';
+SELECT 
+    COUNT(*)
+FROM
+    titles
+WHERE
+    title = 'Enginner'
+        AND to_date = '9999-01-01';
 
 
 -- 문제11
 -- 사번이 13250(Zeydy)인 직원의 직책 변경 상황을 시간순으로 출력해보세요.
-select *
-from titles
-where emp_no = '13250'
-order by from_date;
+SELECT 
+    *
+FROM
+    titles
+WHERE
+    emp_no = '13250'
+ORDER BY from_date;
 
 
 
